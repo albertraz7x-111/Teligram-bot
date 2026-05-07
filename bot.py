3533cc660ab1cfe9566b5ad81dd1dcf105b8eefa
albertraz7x-111 <albertraz7x@gmail.com>
2026-05-08 01:24:11 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,16 @@
-import telebot
 from flask import Flask, send_from_directory
-import threading
 import os
 
-# ======================
-BOT_TOKEN = "8520087047:AAEbINFxeI5dLH68ZEiSVxKvRH2-8Dvo8W8"
-# ======================
-
-bot = telebot.TeleBot(BOT_TOKEN)
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     return "Bot is running"
 
-
 @app.route('/user/<chat_id>')
 def user_page(chat_id):
     return send_from_directory('.', 'index.html')
 
-
-def run_bot():
-    bot.infinity_polling()
-
-
-threading.Thread(target=run_bot).start()
-
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
